9dffc86f12c804617e82e087942edee7af41af58
Vijay Roy <32034872+vijayroy-collab@users.noreply.github.com>
2022-04-12 20:20:16 +0530

    initial build

---
+++ src/components/pages/CodeQuality/CodeQuality.jsx
@@ -0,0 +1,17 @@
+import { Box } from '@material-ui/core'
+import React from 'react'
+
+function CodeQuality() {
+    return (
+        <div className="contact-main">
+			<div className="contact-heading" style={{margin:'3em'}}>
+			<br/>
+            <br/>
+            <br/>
+            <hr/>
+			</div>
+        </div>
+    )
+}
+
+export default CodeQuality
